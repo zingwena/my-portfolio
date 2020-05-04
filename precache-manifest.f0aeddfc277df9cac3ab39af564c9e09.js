d0f29402f0331a5cdcaf916778c78e191ff50a90
Warren <warrenzingwena@gmail.com>
2020-05-04 17:11:39 +0200

    Updates

---
+++ precache-manifest.f0aeddfc277df9cac3ab39af564c9e09.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f012c63d2842c6fb5edd908f35480753",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "6e53374c6e4973cb89f4",
+    "url": "/my-portfolio/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "8931a67c6ef64b323a7e",
+    "url": "/my-portfolio/static/css/main.3ca73889.chunk.css"
+  },
+  {
+    "revision": "6e53374c6e4973cb89f4",
+    "url": "/my-portfolio/static/js/2.6f7a26f7.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/my-portfolio/static/js/2.6f7a26f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8931a67c6ef64b323a7e",
+    "url": "/my-portfolio/static/js/main.783e4d2b.chunk.js"
+  },
+  {
+    "revision": "164cd71dc8e56a933ce3",
+    "url": "/my-portfolio/static/js/runtime-main.d96a2d46.js"
+  },
+  {
+    "revision": "d813aa476abd8fd4974ba6194744a923",
+    "url": "/my-portfolio/static/media/Warren.d813aa47.png"
+  },
+  {
+    "revision": "7299c1f27ad0fb12199e35c7667e82fd",
+    "url": "/my-portfolio/static/media/ehr.7299c1f2.jpg"
+  },
+  {
+    "revision": "fabe582c28d5145ad696451be2ce311e",
+    "url": "/my-portfolio/static/media/flutter.fabe582c.jpg"
+  },
+  {
+    "revision": "2d7905cb5b6ff5bb3faa25227d53717e",
+    "url": "/my-portfolio/static/media/polling.2d7905cb.png"
+  }
+]);
